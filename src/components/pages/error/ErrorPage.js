351340fbacfbec787ceb2ac57c98073681605909
didierLaPasserelle <106524094+didierLaPasserelle@users.noreply.github.com>
2023-01-30 20:11:05 +0100

    ✨ F02: LOGIN > Add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+        <h1>ErrorPage</h1>
+        <Link to="/">
+            <button>Retourner à la page d'accueil</button>
+        </Link>
+        
+    </div>
   )
 }
