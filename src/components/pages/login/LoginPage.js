a77a22d0a107f6be77528b2ac32ac23542b0c1e7
didierLaPasserelle <106524094+didierLaPasserelle@users.noreply.github.com>
2023-01-30 17:46:32 +0100

    ♻️ F01: LOGIN > Create LoginForm

---
+++ src/components/pages/login/LoginPage.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import LoginForm from "./LoginForm"
+
+export default function LoginPage() {
+   
+
+    // render
+
+  return (
+    <LoginForm />
+  )
+}
+
+/* 
+Pour la collecte des données, on met en place un useState que l'on projette sur l'affichage via l'attribut value. C'est la synchro descendante.
+Ensuite la synchro ascendante en gérant le changement
+*/
