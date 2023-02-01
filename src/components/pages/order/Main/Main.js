b0ad84e59a8bccd12b5ba3e28b8dbcf29cd82288
didierLaPasserelle <106524094+didierLaPasserelle@users.noreply.github.com>
2023-02-01 16:03:28 +0100

    F05: ORDER > MAIN > Prepare code for < Basket/> for later

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -5,8 +5,8 @@ import Menu from "./Menu";
 export default function Main() {
   return (
     <MainStyled className="main">
-        <div className="basket">basket</div>
-        <Menu />
+{/*<div className="basket">basket</div>*/}        
+    <Menu />
     </MainStyled>
   )
 }
@@ -20,11 +20,11 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
 
   display: grid; // de base le grid met les éléments les uns sur les autres
-    grid-template-columns: 25% 75%; // ou 1fr (ça prend tout le reste de la place)
+    grid-template-columns: 100%; // ou 1fr (ça prend tout le reste de la place)
 
-  .basket{
+  /* .basket{
     background: pink;
-  }
+  } */
 
 `;
 
