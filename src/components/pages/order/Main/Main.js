7d6de755791f59a11df09470a6580c9115fd652e
didierLaPasserelle <106524094+didierLaPasserelle@users.noreply.github.com>
2023-02-01 16:45:18 +0100

    F05: ORDER > Menu > Add grid layout

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -20,7 +20,9 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
 
   display: grid; // de base le grid met les éléments les uns sur les autres
-    grid-template-columns: 100%; // ou 1fr (ça prend tout le reste de la place)
+  grid-template-columns: 100%; // ou 1fr (ça prend tout le reste de la place)
+
+  overflow-y: scroll; // pour scroller de bas en haut d'où le y
 
   /* .basket{
     background: pink;
--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,14 +1,30 @@
+import { useState } from "react"
 import styled from "styled-components"
+import { fakeMenu1, fakeMenu2 } from "../../../fakedata/fakeMenu";
 
 export default function Menu() {
+    const [menu, setMenu] = useState(fakeMenu2)      
+
   return (
     <MenuStyled className="menu">
-        menu
+        {menu.map((produit) => {
+            return <div className="produit">{produit.title}</div>
+        })}
     </MenuStyled>
-
   )
 }
 
 const MenuStyled = styled.div`
     background: green;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr); // je veux 4 éléments par ligne et je veux qu'ils aient tous la même taille.
+    grid-row-gap: 60px; // pour laisser de l'espace entre les cards au niveau du bas
+    padding: 50px 50px 150px;
+    justify-items: center; // pour centrer la grid
+    
+    .produit{
+        background: red;
+        width: 240px;
+        height: 330px;
+    }
 `;
