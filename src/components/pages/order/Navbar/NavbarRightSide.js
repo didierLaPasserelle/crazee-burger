d7997b2f4c276d0ab6439354b849302f4d8cede6
didierLaPasserelle <106524094+didierLaPasserelle@users.noreply.github.com>
2023-02-08 15:33:18 +0100

    F06: NAVBAR > Right side > Add spacing

--- src/components/pages/order/Navbar/NavbarRightSide.js
+++ src/components/pages/order/Navbar/NavbarRightSide.js
@@ -1,27 +1,22 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable UI/ToggleButton"
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled className="right-side">
-        {/*<div className="admin-button">Admin button</div>*/}
-    <Profile username={username}/>
-      <div className="picture"></div>
-    </NavbarRightSideStyled>  )
+      <ToggleButton 
+        labelIfUnchecked="ACTIVER LE MODE ADMIN"
+        labelIfChecked="DESACTIVER LE MODE ADMIN"
+      />
+      <Profile username={username}/>
+    </NavbarRightSideStyled>
+    )
 }
 
 const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px; // en plus des 20px à gauche et à droite de la navbar, on fait un padding de 50px ici pour avoir les 70px demandés dans le figma
-  
-        
-  /* .admin-button{
-    background: lightblue;
-        } */
-  
-  .profile{
-    background: yellow;
-  }
-  
+
 `;
